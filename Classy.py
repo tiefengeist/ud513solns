9aec161521316bfd7f0c7123dc6a708707f874bc
tiefengeist <43973873+tiefengeist@users.noreply.github.com>
2019-05-22 11:12:09 -0700

    Add files via upload

---
+++ Classy.py
@@ -0,0 +1,14 @@
+class Classy(object):
+    def __init__(self):
+        self.items = []
+    def addItem(self, item):
+        self.items.append(item)
+    def getClassiness(self):
+        totalClassiness = 0
+        classyDict = {"tophat":2,"bowtie":4,"monocle":5}
+        for item in self.items:
+            try:
+                totalClassiness += classyDict[item]
+            except KeyError:
+                pass
+        return totalClassiness
