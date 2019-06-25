03e9064b753e5d95296027159f4008bebb7ecbfb
tiefengeist <sahirnambiar@gmail.com>
2019-06-24 20:59:47 -0700

    added preorder traversal

---
+++ lcsols/binary_preorder.py
@@ -0,0 +1,20 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preOrder(self, root):
+        res, stack = [], [(root, False)]
+        while stack:
+            root, visited = stack.pop()
+            if root is None:
+                continue
+            if visited:
+                res.append(root, val)
+            else:
+                stack.append((root.right, False))
+                stack.append((root.left, False))
+                stack.append((root, True))
+        return res
