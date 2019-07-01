4501df0e142454d9ee541838416cf039dd51dc3d
tiefengeist <sahirnambiar@gmail.com>
2019-07-01 12:05:02 -0700

    solved using BFS

---
+++ lcsols/binary-tree-inversion.py
@@ -0,0 +1,21 @@
+import collections
+
+#Solved using breadth-first
+class Queue(object):
+    def __init__(self):
+        self.data = collections.deque()
+
+    def push(self, x):
+        self.data.append(x)
+
+    def peek(self):
+        return self.data[0]
+
+    def pop(self):
+        return self.data.popleft()
+
+    def size(self):
+        return len(self.data)
+
+    def empty(self):
+        return len(self.data) == 0
