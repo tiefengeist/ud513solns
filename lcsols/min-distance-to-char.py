f667c06108fcbd5ea85be95299275d3a55b1503f
tiefengeist <sahirnambiar@gmail.com>
2019-07-01 12:01:08 -0700

    final problem solution added

---
+++ lcsols/min-distance-to-char.py
@@ -0,0 +1,13 @@
+import itertools
+
+
+class Solution(object):
+    def shortestToChar(self, S, C):
+        result = [len(S)] * len(S)
+        prev = -len(S)
+        for i in itertools.chain(xrange(len(S)),
+                                 reversed(xrange(len(S)))):
+            if S[i] == C:
+                prev = i
+            result[i] = min(result[i], abs(i-prev))
+        return result
