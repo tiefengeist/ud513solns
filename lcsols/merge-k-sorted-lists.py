019374aaa6d2a387a4f5e07ec877849a602d23bb
tiefengeist <sahirnambiar@gmail.com>
2019-07-08 17:59:34 -0700

    used heap to solve merge k sorted arrays problem

---
+++ lcsols/merge-k-sorted-lists.py
@@ -0,0 +1,29 @@
+import heapq
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        if self:
+            return "{} -> {}".format(self.val, self.next)
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        dummy = ListNode(0)
+        current = dummy
+
+        heap = []
+        for sorted_list in lists:
+            if sorted_list:
+                heapq.heappush(heap, (sorted_list.val, sorted_list))
+
+        while heap:
+            smallest = heapq.heappop(heap)[1]
+            current.next = smallest
+            current = current.next
+            if smallest.next:
+                heapq.heappush(heap, (smallest.next.val, smallest.next))
+
+        return dummy.next
