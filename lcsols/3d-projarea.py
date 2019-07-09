564e347ae8b35a07122061467b6116982b8a2573
tiefengeist <sahirnambiar@gmail.com>
2019-07-08 18:02:48 -0700

    found 3d projection area solution in O(n^2) complexity

---
+++ lcsols/3d-projarea.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def projectionArea(self, grid):
+        result = 0
+        for i in xrange(len(grid)):
+            max_row, max_col = 0, 0
+            for j in xrange(len(grid)):
+                if grid[i][j]:
+                    result += 1
+                max_row = max(max_row, grid[i][j])
+                max_col = max(max_col, grid[j][i])
+            result += max_row + max_col
+        return result
